3c87c6d0f1a8a02b1856e62d4abed0d4d6661429
Tycoon19 <kevin960831@naver.com>
2021-12-26 13:00:43 +0900

    양 옆의 여백 공간을 제거  -> 커스텀뷰를 들고 있는 툴바를 불러와 변수에 저장  -> 양 옆의 여백을 설정하는 기능을 이용해 0으로 설정

--- app/src/main/java/com/example/drawblexmlpractice_jaewhi/MainActivity.kt
+++ app/src/main/java/com/example/drawblexmlpractice_jaewhi/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.drawblexmlpractice_jaewhi
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +18,11 @@ class MainActivity : AppCompatActivity() {
 //        실제로 어떤 커스텀뷰를 사용할건지? R. -> res폴더에서 찾아줘라 이런 의미였음~!~!
 //        여기까지만 하면 액션바를 불러오지만 부자연스럽다ㅇㅇ. 양옆이 안채워짐.
         defaultActionBar.setCustomView(R.layout.my_cutom_actionbar)
+
+//        커스텀뷰를 들고 있는 툴바를 변수에 저장.
+        val toolbar = defaultActionBar.customView.parent as Toolbar
+//        절대값으로 설정하는 기능을 불러와 양 옆의 여백을 각각 0으로 설정한다.
+        toolbar.setContentInsetsAbsolute(0 , 0)
 
     }
 }
